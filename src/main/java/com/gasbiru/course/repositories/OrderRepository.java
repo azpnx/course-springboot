06ce6b8c36d30a740c7537caad163786d161d05d
Gabriel Garcez <procurando.gabriel@hotmail.com>
2020-07-03 18:33:39 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/gasbiru/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.gasbiru.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gasbiru.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
