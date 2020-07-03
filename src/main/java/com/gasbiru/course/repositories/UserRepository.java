b22b06f31fd4752dacc5b0be2fac8eb5103b15ca
Gabriel Garcez <procurando.gabriel@hotmail.com>
2020-07-02 23:57:40 -0300

    JPA repository, dependency injection, database seedind

---
+++ src/main/java/com/gasbiru/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gasbiru.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gasbiru.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
